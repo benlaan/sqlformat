22c5c469df7b38c970860e8a102a307a53429fc0
Ben Laan <benlaan@gmail.com>
2009-05-30 00:37:50 +0000

    Fix: updated reference Add: test for update statement (not implemented yet)
    --HG--
    extra : convert_revision : svn%3Adabc8568-4856-0410-9089-d38da34f6efa/trunk%4057

--- Laan.SQL.Formatter.Test/TestAppender.cs
+++ Laan.SQL.Formatter.Test/TestAppender.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using MbUnit.Framework;
 
-using NHibernate.Appender;
+using Laan.NHibernate.Appender;
 
 namespace Laan.SQL.Formatter.Test
 {
---
+++ Laan.SQL.Parser.Test/TestUpdateStatementParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using Laan.SQL.Parser;
+
+namespace Laan.SQLParser.Test
+{
+    [TestFixture]
+    public class TestUpdateStatementParser
+    {
+        //[Test]
+        //public void Test_Basic_Update_Statement()
+        //{
+        //    // Exercise
+        //    UpdateStatement statement = ParserFactory.Execute<UpdateStatement>( "update dbo.table set field = 1" );
+
+        //    // Verify outcome
+        //    Assert.IsNotNull( statement );
+        //    Assert.AreEqual( "dbo.table", statement.TableName );
+        //    Assert.AreEqual( 1, statement.Fields.Count );
+
+        //    Assert.AreEqual( "1", statement.Fields[ 0 ].Expression.Value );
+        //}
+    }
+}
