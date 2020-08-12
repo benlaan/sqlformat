7c979a849cd03280ebf6e4125f1c5a4148e7929e
Ben Laan <benlaan@gmail.com>
2020-08-13 01:44:29 +0930

    Added Blazor WebAssembly in place of 'legacy' aurelia application - not quite working fully yet

---
+++ Laan.Sql.Formatter.Web.Blazor/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Laan.Sql.Formatter.Web.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services
+              .AddBlazorise(options =>
+             {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
+
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Laan.Sql.Formatter.Web.Blazor/wwwroot/js/interop.js
@@ -0,0 +1,27 @@
+﻿window.copyFormatted = function () {
+
+    var formattedCode = document.getElementById("formattedCode");
+
+    var range = document.createRange();
+    range.selectNode(formattedCode);
+    window.getSelection().addRange(range);
+    var success = document.execCommand("copy");
+    window.getSelection().empty();
+
+    return success;
+
+}
+
+window.prettify = function () {
+
+    var formattedCode = document.getElementById("formattedCode");
+    var value = formattedCode.textContent;
+    if (value) {
+
+        pretty = prettyPrintOne(value)
+        this.formattedCode.innerHTML = pretty;
+        return true;
+    }
+
+    return false;
+}
